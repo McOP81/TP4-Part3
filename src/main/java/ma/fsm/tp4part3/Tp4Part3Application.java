f5ca72dfd1c1f6a51900ace65cda6221c3d06ecc
R-ELKADAH <rachidelkadah@email.com>
2024-06-13 21:42:05 +0100

    TP4-Part3

---
+++ src/main/java/ma/fsm/tp4part3/Tp4Part3Application.java
@@ -0,0 +1,13 @@
+package ma.fsm.tp4part3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp4Part3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tp4Part3Application.class, args);
+    }
+
+}
---
+++ src/test/java/ma/fsm/tp4part3/Tp4Part3ApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.fsm.tp4part3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp4Part3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
